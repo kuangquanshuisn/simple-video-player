ad9a2482a5e408d3b1962ca9a10e776fdb901261
kuangquanshuisn <kuangquanshuisn@gmail.com>
2025-07-30 02:49:56 +0000

    feat:从文件读取初始数据，同时保持新增功能

---
+++ pub/data.js
@@ -0,0 +1,29 @@
+// data.js
+const playerData = [
+	  {
+    title: "边水往事",
+    episodes: [
+      { name: "第01集", url: "https://cdn.wlcdn88.com:777/93b2c49b/index.m3u8" },
+      { name: "第02集", url: "https://cdn.wlcdn88.com:777/60e45c45/index.m3u8" },
+      { name: "第03集", url: "https://cdn.wlcdn88.com:777/e1677103/index.m3u8" },
+      { name: "第04集", url: "https://cdn.wlcdn88.com:777/82a1a704/index.m3u8" },
+      { name: "第05集", url: "https://cdn.wlcdn88.com:777/de774b11/index.m3u8" },
+      { name: "第06集", url: "https://cdn.wlcdn88.com:777/270b8986/index.m3u8" },
+      { name: "第07集", url: "https://cdn.wlcdn88.com:777/d4a089a2/index.m3u8" },
+      { name: "第08集", url: "https://cdn.wlcdn88.com:777/ca692cd4/index.m3u8" },
+      { name: "第09集", url: "https://cdn.wlcdn88.com:777/fbe48f15/index.m3u8" },
+      { name: "第10集", url: "https://cdn.wlcdn88.com:777/bdfd35c9/index.m3u8" },
+      { name: "第11集", url: "https://cdn.wlcdn88.com:777/747c8a47/index.m3u8" },
+      { name: "第12集", url: "https://cdn.wlcdn88.com:777/cf81fb3e/index.m3u8" },
+      { name: "第13集", url: "https://cdn.wlcdn88.com:777/2af5ad3e/index.m3u8" },
+      { name: "第14集", url: "https://cdn.wlcdn88.com:777/8df30907/index.m3u8" },
+      { name: "第15集", url: "https://cdn.wlcdn88.com:777/f8ba21cf/index.m3u8" },
+      { name: "第16集", url: "https://cdn.wlcdn88.com:777/7f4f499a/index.m3u8" },
+      { name: "第17集", url: "https://cdn.wlcdn88.com:777/3c4498da/index.m3u8" },
+      { name: "第18集", url: "https://cdn.wlcdn88.com:777/7b8f24a2/index.m3u8" },
+      { name: "第19集", url: "https://cdn.wlcdn88.com:777/6a7d078f/index.m3u8" },
+      { name: "第20集", url: "https://cdn.wlcdn88.com:777/25d4f426/index.m3u8" },
+      { name: "第21集", url: "https://cdn.wlcdn88.com:777/22cafc88/index.m3u8" }
+    ]
+  }
+]
